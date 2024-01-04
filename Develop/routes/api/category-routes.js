ca0e43d9f055bb762a985333e1542ae6fbf05614
Zachary Elbring <elbringzach@gmail.com>
2024-01-04 16:41:36 -0600

    Added get route to category-routes.js.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,22 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const categories = await Category.findAll({
+      include: [{model: Product}],
+    });
+    if(!categories) {
+      res.status(404).json({message: 'No entries found.'});
+      return;
+    }
+    res.status(200).json(categories);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
