6c4bd44866de513cec6c53b38e95c3ac26f9d46e
Zachary Elbring <elbringzach@gmail.com>
2024-01-04 16:54:19 -0600

    Completes routes for category-routes.js.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -21,21 +21,71 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categories = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    if(!categories) {
+      res.status(404).json({message: 'No entries found.'});
+      return;
+    }
+    res.status(200).json(categories);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categories = await Category.create(req.body);
+    res.status(200).json(categories);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategories = await Category.update({
+      category_name: req.body.category_name,    
+    },
+    {
+      where:{
+        id: req.params.id,
+      }
+    }
+    );
+    if(!updateCategories){
+      res.status(404).json({ message: 'No entries found.' });
+      return;
+    }
+    res.status(200).json(updateCategories);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategories = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    return deletedCategories.json(deletedCategory);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
